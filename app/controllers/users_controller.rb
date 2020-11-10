596895e07b4b2de52d4c1e829b8f09db9d63e2f8
denimcouch <alexmata92@ymail.com>
2020-11-10 15:34:55 -0600

    created show_alt route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,21 @@ class UsersController < ApplicationController
     def show
         user = User.find(params[:id])
         render json: user, include: {fishes: {include: {availability: {}}}, bugs: {include: {availability: {}}}, villagers: {}}
-    end 
+    end
 
+    def show_alt
+        user = User.where({name: params[:name]})
+        render json: user, include: {fishes: {include: {availability: {}}}, bugs: {include: {availability: {}}}, villagers: {}}
+    end
+
+    def create
+        new_user = User.create(user_params)
+        render json: new_user, include: {fishes: {include: {availability: {}}}, bugs: {include: {availability: {}}}, villagers: {}}
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :email, :password, :town_name, :hemisphere) 
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :villagers
   resources :bugs
   resources :fish
-  resources :users
+  resources :users, only: [:index, :create]
+  get('/users/:name', to: 'users#show_alt')
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
