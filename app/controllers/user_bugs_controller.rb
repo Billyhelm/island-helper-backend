8cb990a51b68f60c098f5d356f918257acbb596b
denimcouch <alexmata92@ymail.com>
2020-11-12 15:13:50 -0600

    routes for user_bugs created

--- app/controllers/user_bugs_controller.rb
+++ app/controllers/user_bugs_controller.rb
@@ -1,4 +1,24 @@
-class UserBugController < ApplicationController
+class UserBugsController < ApplicationController
+    def index
+        user_bugs = UserBug.all
+        render json: user_bugs
+    end
+
+    def show
+        user_bug = UserBug.find(params[:id])
+        render json: user_bug
+    end
+
+    def create
+        new_user_bug = UserBug.create(post_params)
+        user = User.find(new_user_bug.user_id)
+        render json: user, include: {fishes: {include: {availability: {}}}, bugs: {include: {availability: {}}}, villagers: {} }
+    end
+
+    def destroy
+        user_bug = UserBug.find(params[:id])
+        user_bug.destroy
+    end
 
     private
     def post_params
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :fish
   resources :users, only: [:index, :create]
   resources :user_villagers
+  resources :user_bugs
   get('/users/:name', to: 'users#show_alt')
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
