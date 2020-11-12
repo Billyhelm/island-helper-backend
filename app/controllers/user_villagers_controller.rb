ef3118b2a3f0c09f3366b53e83c540d281fb804b
denimcouch <alexmata92@ymail.com>
2020-11-12 11:20:14 -0600

    create user_villagers

--- app/controllers/user_villagers_controller.rb
+++ app/controllers/user_villagers_controller.rb
@@ -1,9 +1,24 @@
-class UserVillagerController < ApplicationController
+class UserVillagersController < ApplicationController
 
+    def index
+        user_villagers = UserVillager.all
+        render json: user_villagers
+    end
 
+    def create
+        new_user_villager = UserVillager.create(post_params)
+        user = User.find(new_user_villager.user_id)
+        # byebug
+        render json: user, include: {fishes: {include: {availability: {}}}, bugs: {include: {availability: {}}}, user_villagers: {include: {villager: {}}} }
+    end
+
+    def delete
+
+    end
 
     private
     def post_params
+        # byebug
         params.require(:user_villager).permit(:user_id, :villager_id)
     end
 
